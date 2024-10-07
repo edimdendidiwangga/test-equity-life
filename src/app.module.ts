9685a66dfaa4d56c03c011bb68d8fc382f3de50c
edimdendidiwangga <edimdendy@gmail.com>
2024-10-07 12:33:43 +0700

    set typoeTypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      url: 'mysql://root:vjiqkBBiLERTtdPVFIpfpTpiYjCCuwYj@autorack.proxy.rlwy.net:46960/railway',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    EmployeeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
