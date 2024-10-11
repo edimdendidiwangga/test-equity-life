b9d384e8f5ce9dda6f7d4781bd867c4d6aa9e190
edimdendidiwangga <edimdendy@gmail.com>
2024-10-11 12:00:53 +0700

    Export TypeOrmModule to make repositories available to other modules

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Employee } from './employee.entity';
-import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Employee])],
-  providers: [EmployeeService],
   controllers: [EmployeeController],
+  providers: [EmployeeService],
+  exports: [TypeOrmModule],
 })
 export class EmployeeModule {}
