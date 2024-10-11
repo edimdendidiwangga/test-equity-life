b9c260cb6d8ceccab9e622db298cd7a7e7e7b7c8
edimdendidiwangga <edimdendy@gmail.com>
2024-10-11 11:58:39 +0700

    get employee hirarchy

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -7,10 +7,34 @@ import { Employee } from './employee.entity';
 export class EmployeeService {
   constructor(
     @InjectRepository(Employee)
-    private readonly employeeRepository: Repository<Employee>,
+    private employeeRepository: Repository<Employee>,
   ) {}
 
   async getAllEmployees(): Promise<Employee[]> {
-    return this.employeeRepository.find();
+    const employees = await this.employeeRepository.find();
+    return this.buildEmployeeHierarchy(employees);
+  }
+
+  private buildEmployeeHierarchy(employees: Employee[]): any {
+    const hierarchy = [];
+    const employeeMap = new Map();
+
+    // Build a map of employees by their IDs
+    employees.forEach((employee) => {
+      employeeMap.set(employee.employee_id, { ...employee, subordinates: [] });
+    });
+
+    // Build the hierarchical structure
+    employees.forEach((employee) => {
+      if (employee.employee_manager_id) {
+        employeeMap
+          .get(employee.employee_manager_id)
+          .subordinates.push(employeeMap.get(employee.employee_id));
+      } else {
+        hierarchy.push(employeeMap.get(employee.employee_id));
+      }
+    });
+
+    return hierarchy;
   }
 }
